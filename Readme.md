85a4c112bf13bbaf7126f5edf03c09df617e91c4
Ashwini <akamzing412@gmail.com>
2024-05-11 17:30:13 +0530

    learning routes and middleware in express

--- Readme.md
+++ Readme.md
@@ -42,3 +42,23 @@ Server is kind of funtion which runs once after every request.
 
 
 ExpressJs --> 
+
+
+
+
+Browser can only do get requests
+
+
+
+Middleware  --> Server k entry and client k request k bich ka operations
+
+
+Middleware functions are functions that have access to the request object (req), the response object (res), and the next middleware function in the application’s request-response cycle. The next middleware function is commonly denoted by a variable named next.
+
+Middleware functions can perform the following tasks:
+
+Execute any code.
+Make changes to the request and the response objects.
+End the request-response cycle.
+Call the next middleware function in the stack.
+If the current middleware function does not end the request-response cycle, it must call next() to pass control to the next middleware function. Otherwise, the request will be left hanging.
--- index.js
+++ index.js
@@ -1,43 +1,75 @@
-import http from 'http';
 import fs from 'fs';
-
+import express from 'express';
 
 
 const index = fs.readFileSync('index.html', 'utf-8')
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
-
 const products = data.products;
 
 
 
+const app = express();
 
-const server = http.createServer((req, res) => {
-    if (req.url.startsWith('/product')) {
-        const id = req.url.split('/')[2]
-        const prd = products.find(p => p.id === (+id))
-        res.setHeader('Content-Type', 'text/html')
-        let ModifiedIndex = index.replace('**title**', prd.title).replace('**url**', prd.thumbnail)
-        console.log(prd);
-        res.end(ModifiedIndex)
-        return;
-    }
-    switch (req.url) {
-        case '/':
-            res.setHeader('Content-Type', 'text/html')
-            res.end(index)
-            break;
-        case '/data':
-            res.setHeader('Content-Type', 'application/json')
-            res.end(JSON.stringify(data))
-            break;
-        default:
-            res.writeHead(404, "Page not found")
-            res.end()
-            break;
-    }
-    console.log("Server started on port 8080");
-    console.log(req.url);
+app.use((req,res,next)=>{
+    console.log(req.method, req.ip, req.hostname,new Date() , req.get('User-Agent'));
+    next()
+});
 
+
+
+
+const auth = ((req,res,next)=>{
+    console.log("Inside Auth");
+    console.log(req.query);
+    if(req.query.password == 12345){}
+    else{
+        res.sendStatus(401);
+    }
+    next()
+});
+
+
+//Auth middleware will be used on all paths
+//app.use(auth)
+
+
+// API -- End Point 
+
+//If we pass auth here
+app.get('/',auth, (req, res) => {
+    console.log("Get Called");
+    res.json({type:"GET"})
+})
+app.post('/',auth, (req, res) => {
+    res.json({type:"POST Successfull"})
+})
+app.put('/', (req, res) => {
+    res.json({type:"PUT"})
+})
+app.delete('/', (req, res) => {
+    res.json({type:"DELETE"})
+})
+app.patch('/', (req, res) => {
+    res.json({type:"PATCH"})
 })
 
-server.listen(8080)
+
+
+
+
+
+
+
+app.get('/demo', (req, res) => {
+    // res.send("<h1>Hello</h1>");
+    // res.json(data)
+    // res.sendFile('D:/PROJECTS/Learn-NodeJs/index.html')
+})
+
+
+
+
+
+app.listen(8080, () =>{
+    console.log('Server is listening on port 8080');
+})
