58164db1322d604f99a808e27f1db20408d02bc7
ASHWINI <akamazing412@gmail.com>
2024-05-13 20:11:10 +0530

    static fixed

--- index.js
+++ index.js
@@ -5,18 +5,11 @@ import productRouter from './routes/product.js';
 import userRouter from './routes/user.js';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path';
-
 const app = express();
 
 //Body Parser
 dotenv.config();
-const port  = process.env.PORT || 8080;
-// console.log('env : ',process.env.DB_PASSWORD);
-
-
+const port  = process.env.PORT;
 async function connectToDatabase() {
     try {
         await mongoose.connect(process.env.DB_URL);
@@ -25,27 +18,15 @@ async function connectToDatabase() {
         console.error("Database connection error:", error);
     }
 }
-
-// Call the async function
 connectToDatabase();
-
-
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 app.use(cors());
 
 app.use(express.json());
 //To print details of api calls
 app.use(morgan('combined'));
-app.use(express.static(process.env.PUBLIC_DIR));
 app.use('/products',productRouter)
 app.use('/users',userRouter)
-app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-})
 app.listen(port, () => {
-    console.log('Server is listening on port 8080');
+    console.log('Server is listening on port',port);
 })
