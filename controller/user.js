1086ddb3783f31331baa77498c93dbd1b157f53d
ASHWINI <akamazing412@gmail.com>
2024-05-13 17:16:30 +0530

    vercel config fixed

--- controller/user.js
+++ controller/user.js
@@ -1,8 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 const index = fs.readFileSync('index.html', 'utf-8')
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-const data = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../data.json'), 'utf-8'))
+const data = JSON.parse(fs.readFileSync(path.resolve('./data.json'), 'utf-8'))
 const users = data.users;
 
 //CREATE POST /users
